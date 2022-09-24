4e3eb25c3caaa1749680c92ce690054385c13634
0623-github <2277044572@qq.com>
2022-09-24 17:35:12 +0800

    fixed get param error and add result for response

--- handler/userApi.go
+++ handler/userApi.go
@@ -21,6 +21,7 @@ type signUpResponse struct {
 	Code int `json:"code"`
 	Message string `json:"message"`
 	AccountNumber string `json:"account_number"`
+	Result bool `json:"result"`
 }
 
 // SignUp func
@@ -29,6 +30,7 @@ func SignUp(ctx *gin.Context) {
 	resp := signUpResponse{
 		Code: util.StatusSuccess,
 		Message: "success",
+		Result: true,
 	}
 	fmt.Println(ctx)
 	err := ctx.BindJSON(req)
@@ -36,6 +38,7 @@ func SignUp(ctx *gin.Context) {
 		fmt.Println(err)
 		resp.Code = util.StatusParamErr
 		resp.Message = "注册参数错误"
+		resp.Result = false
 	} else {
 		u := signUpReqToUser(req)
 		util.DistributeUserAccount(u)
@@ -59,6 +62,7 @@ type signInResponse struct {
 	AccountNumber string `json:"account_number"`
 	NickName string `json:"nick_name"`
 	Grade int `json:"grade"`
+	Result bool `json:"result"`
 }
 
 func SignIn(ctx *gin.Context) {
@@ -66,29 +70,29 @@ func SignIn(ctx *gin.Context) {
 	resp := &signInResponse{
 		Code: util.StatusSuccess,
 		Message: "登陆成功",
+		Result: true,
 	}
-	err := ctx.BindJSON(req)
-	if err != nil {
-		fmt.Println(err)
-		resp.Code = util.StatusParamErr
-		resp.Message = "注册参数错误"
+	req.AccountNumber = ctx.Query("account_number")
+	req.Pwd = ctx.Query("pwd")
+	fmt.Println(req)
+	user := &model.User{
+		AccountNumber: req.AccountNumber,
+	}
+	sql.SelectUserByAccount(user)
+	fmt.Println(user.Pwd, req.Pwd)
+	if req.Pwd == "" {
+		resp.Code = util.StatusPwdErr
+		resp.Message = "密码为空"
+		resp.Result = false
+	} else if user.Pwd != req.Pwd {
+		resp.Code = util.StatusPwdErr
+		resp.Message = "密码错误，请重试"
+		resp.Result = false
 	} else {
-		user := &model.User{
-			AccountNumber: req.AccountNumber,
-		}
-		sql.SelectUserByAccount(user)
-		fmt.Println(user.Pwd, req.Pwd)
-		if req.Pwd == "" {
-			resp.Code = util.StatusPwdErr
-			resp.Message = "密码为空"
-		} else if user.Pwd != req.Pwd {
-			resp.Code = util.StatusPwdErr
-			resp.Message = "密码错误，请重试"
-		} else {
-			resp.NickName = user.NickName
-			resp.Grade = user.Grade
-			resp.AccountNumber = user.AccountNumber
-		}
+		resp.NickName = user.NickName
+		resp.Grade = user.Grade
+		resp.AccountNumber = user.AccountNumber
+		resp.Result = true
 	}
 	ctx.JSON(200, resp)
 }
